5bcdd8397d8dde110672ca93a258cf151468055b
OlamideHawanat123 <raheemhawanat@gmail.com>
2025-04-11 12:11:09 +0100

    first committ

---
+++ fridayTask6.js
@@ -0,0 +1,14 @@
+const products = [
+    {id: 1, name: "laptop", price: 1200},
+    {id: 2, name: "phone", price: 800},
+    {id: 3, name: "tablet", price: 500},
+    {id: 4, name: "desktop", price: 700},
+
+]
+
+
+let thresholdAmount = 700;
+const findExpensiveProducts = function(products){
+
+
+}
